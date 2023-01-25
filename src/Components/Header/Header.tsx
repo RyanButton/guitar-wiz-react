5933ed367c4f4df74212fd322e61e1acbe7174d0
Ryan Button <ryan@oneblink.io>
2023-01-26 02:12:44 +1100

    minor UI fixes

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,10 +3,24 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const LogoWrapper = styled.div`
+  text-align: center;
+  font-size: 22px;
+  padding: 0.5rem 0 0.5rem 0;
   border-radius: 5px;
   background-color: white;
-  margin-bottom: 5px;
 
+  a {
+    text-decoration: none;
+    &:hover,
+    &:visited,
+    &:active {
+      color: #000;
+    }
+  }
+  a > h1 {
+    margin 0;
+    font-family: 'New Rocker', cursive;
+  }
   img {
     display: block;
     margin-left: auto;
@@ -14,9 +28,7 @@ const LogoWrapper = styled.div`
   }
 
   @media only screen and (max-width: 700px) {
-    img {
-      max-width: 60%;
-    }
+    font-size: 16px;
   }
 `
 
@@ -25,7 +37,7 @@ function Header() {
     <div className="header-wrapper">
       <LogoWrapper>
         <Link to="/">
-          <img src="/images/logo3.png" alt="Guitar Wiz" />
+          <h1>Guitar Wiz</h1>
         </Link>
       </LogoWrapper>
       <NavBar />
